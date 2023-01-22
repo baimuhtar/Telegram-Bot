0e9337c1ed74acaa496018e912139cb7397e7ad2
baimuhtar <baimuhtar1997@gmail.com>
2023-01-22 20:50:12 +0600

    Creating model

---
+++ src/main/java/io/proj3ct/OSI_Mars_Bot/model/User.java
@@ -0,0 +1,60 @@
+package io.proj3ct.OSI_Mars_Bot.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.sql.Timestamp;
+
+@Entity(name="usersDataTable")
+public class User {
+    @Id
+    private Long chatId;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String userName;
+
+    private Timestamp timestamp;
+
+    public Long getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(Long chatId) {
+        this.chatId = chatId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+}
---
+++ src/main/java/io/proj3ct/OSI_Mars_Bot/model/UserRepository.java
@@ -0,0 +1,8 @@
+package io.proj3ct.OSI_Mars_Bot.model;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
